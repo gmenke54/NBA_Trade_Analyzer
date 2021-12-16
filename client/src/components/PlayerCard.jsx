16eca43cee716c7470857bd43346553ebfdea399
gmenke54 <menke.gw@gmail.com>
2021-12-16 11:53:13 -0800

    succesfully displayed teams and correspondng player

---
+++ client/src/components/PlayerCard.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const PlayerCard = (props) => {
+  return (
+    <div className="playerCard">
+      <h3>{props.name}</h3>
+    </div>
+  )
+}
+
+export default PlayerCard
--- client/src/components/Roster.jsx
+++ client/src/components/Roster.jsx
@@ -1,9 +1,20 @@
 import React from "react";
+import PlayerCard from "./PlayerCard";
 
 const Roster = (props) => {
   return (
-    <div>
-      this be the roster bruv
+    <div className="roster">
+      <h2>Players:</h2>
+      {props.players.map((player) => {
+        {
+          return (
+            <PlayerCard
+              key={player._id}
+              name={player.name}
+              />
+          )
+        }
+      })}
     </div>
   )
 }
