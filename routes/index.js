5e0a217875328bb4afc54bf93e04ae1d66cf645c
gmenke54 <menke.gw@gmail.com>
2021-12-16 09:11:46 -0800

    created all routes for posting and getting

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,15 @@
 const { Router } = require('express');
+const controllers = require('../controllers');
 const router = Router();
 
 router.get('/', (req, res) => res.send('This is root!'));
+
+router.get('/players', controllers.getAllPlayers);
+
+router.get('/teams/details/:_id', controllers.getTeamById);
+
+router.post('/teams', controllers.createTeam);
+
+router.post('/players', controllers.createPlayer);
 
 module.exports = router;
