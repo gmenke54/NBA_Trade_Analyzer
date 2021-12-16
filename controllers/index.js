525e202c0348c88bf5d2086a8a4d5da1e5146940
gmenke54 <menke.gw@gmail.com>
2021-12-16 08:58:37 -0800

    install and imported body parser and created route for getting all players

--- controllers/index.js
+++ controllers/index.js
@@ -35,9 +35,18 @@ const getTeamById = async (req, res) => {
     return res.status(500).send(error.message);
   }
 };
+const getAllPlayers = async (req, res) => {
+  try {
+    const players = await Player.find();
+    return res.status(200).json({ players });
+  } catch (error) {
+    return res.status(500).send(error.message);
+  }
+};
 
 module.exports = {
   createTeam,
   createPlayer,
-  getTeamById
+  getTeamById,
+  getAllPlayers
 };
--- server.js
+++ server.js
@@ -13,6 +13,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(logger('dev'));
+app.use(bodyParser.json());
 // app.use() middleware here ^ ///////////////////
 
 app.use('/api', routes);
