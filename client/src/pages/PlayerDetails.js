dc1eb082f37e3da86194626670a31c9b08d4658c
gmenke54 <menke.gw@gmail.com>
2021-12-17 14:54:05 -0800

    added conditional rendering for height and weight depending on whether those stats are in the db

--- client/src/pages/PlayerDetails.js
+++ client/src/pages/PlayerDetails.js
@@ -15,19 +15,32 @@ const PlayerDetails = (props) => {
   useEffect(() => {
     getPlayer();
   }, [props.match.params.name]);
-  return ply ? (
-    <div>
-      <h1>
-        {ply.first_name} {ply.last_name} - {ply.position}
-      </h1>
-      <h3>{ply.team.full_name}</h3>
-      <h4>
-        Height: {ply.height_feet}' {ply.height_inches}"
-      </h4>
-    </div>
-  ) : (
-    <div>Player Not found</div>
-  );
+  if (ply) {
+    return (
+      <div>
+        {ply.height_feet ? (
+          <div>
+            <h1>
+              {ply.first_name} {ply.last_name} - {ply.position}
+            </h1>
+            <h3>{ply.team.full_name}</h3>
+            <h4>
+              {ply.height_feet}' {ply.height_inches}" - {ply.weight_pounds}lbs
+            </h4>
+          </div>
+        ) : (
+          <div>
+            <h1>
+              {ply.first_name} {ply.last_name} - {ply.position}
+            </h1>
+            <h3>{ply.team.full_name}</h3>
+          </div>
+        )}
+      </div>
+    );
+  } else {
+    return <div>Player Not Found</div>;
+  }
 };
 
 export default PlayerDetails;
