980c2786e32b1cf4932c2033fd727014d66d11a9
gmenke54 <menke.gw@gmail.com>
2021-12-16 16:00:55 -0800

    reached MVP

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -6,7 +6,7 @@ const About = () => {
       <h3>This a fantasy basketball trade analyzer for the NBA.</h3>
       <div className="our-about">
         <h3>
-          This full CRUD MERN application was built by{' '}
+          This full CRUD MERN application was built by
           <a href="https://www.linkedin.com/in/grantmenke/">Grant Menke</a>
         </h3>
       </div>
