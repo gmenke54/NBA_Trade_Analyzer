4c301aa35c1b3024d9010922932f6878cd1b7d77
gmenke54 <menke.gw@gmail.com>
2021-12-18 21:38:30 -0800

    began working on the trade results page; made a gameplan

--- client/src/components/TradeResults.jsx
+++ client/src/components/TradeResults.jsx
@@ -1,2 +1,47 @@
 // https://www.balldontlie.io/api/v1/season_averages?player_ids[]=237 <- AXIOS CALL FOR GETTING PLAYERS CURRENT SEASON AVERAGES BY THEIR EXTERNAL API's ID
-// need to make their ID in my DB the same as their ID in the external API
+
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+const TradeResults = (props) => {
+  const [aPlayers, setAPlayers] = useState([])
+  const [bPlayers, setBPlayers] = useState([])
+  const [aStats, setAStats] = useState([])
+  const [bStats, setBStats] = useState([])
+  
+  const getTeams = async () => {
+    const resA = await axios.get(`http://localhost:3001/api/players/${props.teamA_id}`)
+    const resB = await axios.get(`http://localhost:3001/api/players/${props.teamB_id}`)
+    setAPlayers(resA.data.players)
+    setBPlayers(resB.data.players)
+    console.log(resA.data.players)
+    console.log(resB.data.players)
+    const apiCall = aPlayers.reduce((acc, player) => {
+      let plyId = player.api_Id.toString()
+      acc += `player_ids[]=${plyId}&`
+      return acc
+    }, 'https://www.balldontlie.io/api/v1/season_averages?')
+    const res = await axios.get(`${apiCall}`)
+    console.log(res.data.data)
+  }
+
+  // PLAN:
+  // 1. Fix bug with the above api call returning an empty array after refreshing the page (need to load the players prior getting the stats)
+  // 2. Duplicate the process used to get aStats for bStats
+  // 3. Write a new propiertary algorithm that compares bStats to aStats and determines the better trade package
+  // 4. conditionally render the better team in the returned div
+  // 5. add explanation to the analysis
+
+  useEffect(() => {
+    getTeams()
+  }, [props.params])
+  
+  return (
+    <div>
+      <h1>Analysis</h1>
+      <h2>TeamA: {props.teamA_id}</h2>
+    </div>
+  )
+}
+
+export default TradeResults
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -3,15 +3,17 @@ import axios from 'axios';
 import { useHistory } from 'react-router-dom';
 import TeamBox from '../components/TeamBox';
 import AddTeam from '../components/AddTeam';
+import TradeResults from '../components/TradeResults';
 
 export default function Home(props) {
   // let history = useHistory();
   const [teams, setTeams] = useState([]);
   const [teamsLoaded, setTeamsLoaded] = useState(false);
+  const [renderResults, setRenderResults] = useState(false);
 
   useEffect(() => {
     getTeams();
-  }, []);
+  }, [renderResults]);
 
   const getTeams = async () => {
     const res = await axios.get(`http://localhost:3001/api/teams`);
@@ -21,6 +23,10 @@ export default function Home(props) {
     } else {
       setTeamsLoaded(false);
     }
+  };
+
+  const analyze = () => {
+    setRenderResults(true);
   };
 
   if (teamsLoaded === true) {
@@ -44,25 +50,33 @@ export default function Home(props) {
             />
           </div>
         ) : (
-          <div className="teamBoxesCont">
-            <TeamBox
-              {...props}
-              teams={teams}
-              setTeams={setTeams}
-              key={teams[0]._id}
-              team_Name={teams[0].team_Name}
-              manager_Name={teams[0].manager_Name}
-              team_Id={teams[0]._id}
-            />
-            <TeamBox
-              {...props}
-              teams={teams}
-              setTeams={setTeams}
-              key={teams[1]._id}
-              team_Name={teams[1].team_Name}
-              manager_Name={teams[1].manager_Name}
-              team_Id={teams[1]._id}
-            />
+          <div>
+            <div className="teamBoxesCont">
+              <TeamBox
+                {...props}
+                teams={teams}
+                setTeams={setTeams}
+                key={teams[0]._id}
+                team_Name={teams[0].team_Name}
+                manager_Name={teams[0].manager_Name}
+                team_Id={teams[0]._id}
+              />
+              <TeamBox
+                {...props}
+                teams={teams}
+                setTeams={setTeams}
+                key={teams[1]._id}
+                team_Name={teams[1].team_Name}
+                manager_Name={teams[1].manager_Name}
+                team_Id={teams[1]._id}
+              />
+            </div>
+            <button onClick={analyze}>Analyze Trade</button>
+            {renderResults === true ? (
+              <div>
+                <TradeResults teamA_id={teams[0]._id} teamB_id={teams[1]._id} />
+              </div>
+            ) : null}
           </div>
         )}
       </div>
