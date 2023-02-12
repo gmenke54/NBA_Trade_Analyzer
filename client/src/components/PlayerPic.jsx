d5b97b64be68624f0d321125b0ada943177e4471
carlmenke <carlmenke13@gmail.com>
2023-02-11 16:17:00 -0800

    removed another instance of nba.net axios call

--- client/src/components/PlayerPic.jsx
+++ client/src/components/PlayerPic.jsx
@@ -1,15 +1,17 @@
 import React, { useEffect, useState } from "react";
 import axios from 'axios'
 import anon from '../anon2.png'
+import allPlayers from '../data/all_players.json'
 
 const PlayerPic = (props) => {
   const [personId, setPersonId] = useState(null)
 
   useEffect( async () => {
-    const res = await axios.get(
-      `https://data.nba.net/data/10s/prod/v1/2022/players.json`
-    )
-    res.data.league.standard.map((player) => {
+
+    // const res = await axios.get(
+    //   `https://data.nba.net/data/10s/prod/v1/2022/players.json`
+    // )
+    allPlayers.league.standard.map((player) => {
       if (props.last === player.lastName && props.first === player.firstName){
         setPersonId(`https://ak-static.cms.nba.com/wp-content/uploads/headshots/nba/latest/260x190/${player.personId}.png`)
       }
