9d7c2286807d76ff528f7f5a46a988665528537a
carlmenke <carlmenke13@gmail.com>
2023-02-11 16:12:49 -0800

    removed axios calls to data.nba.net

--- client/src/components/PlayerCard.jsx
+++ client/src/components/PlayerCard.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import anon from '../anon2.png'
 import '../css/PlayerCard.css'
+import allPlayers from '../data/all_players.json'
 
 import { BASE_URL } from '../globals'
 
@@ -11,10 +12,10 @@ const PlayerCard = (props) => {
   const [nbaTeam, setNbaTeam] = useState(null)
 
   useEffect( async () => {
-    const res = await axios.get(
-      `https://data.nba.net/data/10s/prod/v1/2022/players.json`
-    )
-    res.data.league.standard.map((player) => {
+    // const res = await axios.get(
+    //   `https://data.nba.net/data/10s/prod/v1/2022/players.json`
+    // )
+    allPlayers.league.standard.map((player) => {
       if (props.name === `${player.firstName} ${player.lastName}`){
         setPersonId(`https://ak-static.cms.nba.com/wp-content/uploads/headshots/nba/latest/260x190/${player.personId}.png`)
       }
--- client/src/components/SugCard.jsx
+++ client/src/components/SugCard.jsx
@@ -1,15 +1,16 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import SugCardDisp from "./SugCardDisp";
+import allPlayers from "../data/all_players.json"
 
 const SugCard = (props) => {
   const [curPlayers, setCurPlayers] = useState(null)
 
   useEffect( async () => {
-    const res = await axios.get(
-      `https://data.nba.net/data/10s/prod/v1/2022/players.json`
-    )
-    setCurPlayers(res.data.league.standard)
+    // const res = await axios.get(
+    //   `https://data.nba.net/data/10s/prod/v1/2022/players.json`
+    // )
+    setCurPlayers(allPlayers.league.standard)
   }, [props.params])
 
   return (
